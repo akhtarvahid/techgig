caac4ddc37baabadb55673812111c87f8d1bc8e5
Vahid Akhtar <a845260@yara.com>
2024-05-04 14:41:09 +0530

    mapStateToProps removed and useSelector used

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-import React from 'react';
-import {Route, Routes } from 'react-router-dom';
-import { connect } from 'react-redux';
-import './index.css';
-import Home from './components/home/Home';
-import Login from './components/login/Login';
-import PrivateRoute from './routing/PrivateRoute';
-import {loginUser} from './actions/auth';
-import { withRouter } from './components/common/withRouter';
-function App(props){
-    const { isLoggedIn } = props.status;
-     return (
-        <>
-          <Routes>
-            <Route exact path="/" element={<Login />} />   
-            <Route path='/home' element={<PrivateRoute  authed={isLoggedIn}>
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import "./index.css";
+import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+import PrivateRoute from "./routing/PrivateRoute";
+function App() {
+  const { isLoggedIn } = useSelector((state) => state.credential);
+  return (
+    <>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute authed={isLoggedIn}>
               <Home />
-            </PrivateRoute>} />
-          </Routes>
-        </>
-    )
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </>
+  );
 }
-function mapStateToProps(state) {
-  return {
-      status: state.credential
-    }
-  }
-export default connect(
-    mapStateToProps, 
-    {loginUser})
-    (withRouter(App)
-);
+
+export default App;
