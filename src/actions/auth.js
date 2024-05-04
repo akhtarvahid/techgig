c903d2b442796172da8f7798e3d49d19287f15c5
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-05-04 16:50:43 +0530

    constant for actions

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,30 +1,29 @@
+import { LOGIN_ID, LOGIN_PASS } from "./constant";
 import { LOGIN, LOGOUT } from "./types";
 
-const loginId = "foo";
-const loginPassword = "bar"
 export const loginUser = (item, navigate) => {
-  let isLoggedIn = false
+  let isLoggedIn = false;
 
-  if(item.username === loginId && item.password === loginPassword) {
-    localStorage.setItem('active', true);
+  if (item.username === LOGIN_ID && item.password === LOGIN_PASS) {
+    localStorage.setItem("active", true);
     isLoggedIn = true;
-    navigate('/home');
-}
+    navigate("/home");
+  }
   return {
     type: LOGIN,
-    payload: isLoggedIn
+    payload: isLoggedIn,
   };
 };
 export const logoutUser = (history) => {
-  localStorage.removeItem('active');
-  history('/')
+  localStorage.removeItem("active");
+  history("/");
   return {
     type: LOGOUT,
-    payload: false
+    payload: false,
   };
 };
 export const isLoggedIn = () => {
   return {
-    type: 'IS_LOGGED_IN'
-  }
-}
+    type: "IS_LOGGED_IN",
+  };
+};
